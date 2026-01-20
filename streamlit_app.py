b6817ca4009099f521b3b5745711f2b28df98eca
Elías Robles <149778129+eliasrrobles@users.noreply.github.com>
2026-01-20 09:24:10 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,15 +10,15 @@ tab1, tab2, tab3 = st.tabs(["Una entrada", "Dos entradas", "Tres entradas y sesg
 with tab3:
     col1, col2, col3 = st.columns(3)
 
-    t3_w0 = col1.number_input("Peso w0", value=0.0, format="%.2f")
-    t3_w1 = col2.number_input("Peso w1", value=0.0, format="%.2f")
-    t3_w2 = col3.number_input("Peso w2", value=0.0, format="%.2f")
+    t3_w0 = col1.slider("Peso w0", value=0.0, format="%.2f")
+    t3_w1 = col2.slider("Peso w1", value=0.0, format="%.2f")
+    t3_w2 = col3.slider("Peso w2", value=0.0, format="%.2f")
 
     t3_x0 = col1.number_input("Entrada x0", value=0.0, format="%.2f")
     t3_x1 = col2.number_input("Entrada x1", value=0.0, format="%.2f")
     t3_x2 = col3.number_input("Entrada x2", value=0.0, format="%.2f")
 
-    t3_b = col1.number_input("Sesgo b", value=0.0, format="%.2f")
+    t3_b = st.number_input("Sesgo b", value=0.0, format="%.2f")
 
     t3_result = t3_w0 * t3_x0 + t3_w1 * t3_x1 + t3_w2 * t3_x2 + t3_b
     if st.button("Calcular salida (tres entradas y sesgo)"):
