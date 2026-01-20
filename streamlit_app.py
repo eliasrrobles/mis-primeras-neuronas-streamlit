b6bf49ad12adb36f511c645b7e80d89e3818bbee
Elías Robles <149778129+eliasrrobles@users.noreply.github.com>
2026-01-20 09:24:59 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,9 +10,9 @@ tab1, tab2, tab3 = st.tabs(["Una entrada", "Dos entradas", "Tres entradas y sesg
 with tab3:
     col1, col2, col3 = st.columns(3)
 
-    t3_w0 = col1.slider("Peso w0", value=0.0, format="%.2f")
-    t3_w1 = col2.slider("Peso w1", value=0.0, format="%.2f")
-    t3_w2 = col3.slider("Peso w2", value=0.0, format="%.2f")
+    t3_w0 = col1.slider("Peso w0", value=0.0, min_value=-10.0, max_value=10.0, format="%.2f")
+    t3_w1 = col2.slider("Peso w1", value=0.0, min_value=-10.0, max_value=10.0, format="%.2f")
+    t3_w2 = col3.slider("Peso w2", value=0.0, min_value=-10.0, max_value=10.0, format="%.2f")
 
     t3_x0 = col1.number_input("Entrada x0", value=0.0, format="%.2f")
     t3_x1 = col2.number_input("Entrada x1", value=0.0, format="%.2f")
